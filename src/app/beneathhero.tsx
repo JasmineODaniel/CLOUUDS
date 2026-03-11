b7530ba45858dde31682aabda55a21ae55bd8d53
THENEVERDONE <163352801+JasmineODaniel@users.noreply.github.com>
2026-03-11 07:28:02 -0700

    Update beneathhero.tsx

--- src/app/beneathhero.tsx
+++ src/app/beneathhero.tsx
@@ -2,7 +2,7 @@ import styles from "./beneathhero.module.css";
 
 const logos = [
   "/logos 01.svg", 
-  "/logos 02.svg",
+  "/logos 02.svg", 
   "/logos 03.svg",
   "/logos 04.svg",
   "/logos 05.svg",
