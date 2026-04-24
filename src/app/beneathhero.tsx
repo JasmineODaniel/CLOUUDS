f6074a761fe8b46445764db993d57e111d34b701
THENEVERDONE <163352801+JasmineODaniel@users.noreply.github.com>
2026-04-24 09:11:07 -0700

    Fix logo array formatting in beneathhero.tsx

--- src/app/beneathhero.tsx
+++ src/app/beneathhero.tsx
@@ -2,7 +2,7 @@ import styles from "./beneathhero.module.css";
 
 const logos = [
   "/logos 06.svg",
-  "/logos 05.svg",
+  "/logos 05.svg", 
   "/logos 04.svg",
   "/logos 03.svg",
   "/logos 02.svg",
