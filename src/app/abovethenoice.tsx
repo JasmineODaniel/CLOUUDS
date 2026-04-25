168a423b3d952fa247073da83fe108cf46af1cfc
THENEVERDONE <163352801+JasmineODaniel@users.noreply.github.com>
2026-04-25 20:50:49 +0100

    Fix import statement formatting in abovethenoice.tsx

--- src/app/abovethenoice.tsx
+++ src/app/abovethenoice.tsx
@@ -1,4 +1,4 @@
- import "./abovethenoice.css";
+  import "./abovethenoice.css";
 
 const StarIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none"> 
